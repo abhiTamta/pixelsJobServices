bf3da6bec2c3f0dd23bbb9b4a4d54441bcba40b9
abhiTamta <abhinay.tamta@hotmail.com>
2025-06-25 09:02:43 +0530

    add footer

--- pixelsjob_web/src/App.jsx
+++ pixelsjob_web/src/App.jsx
@@ -1,8 +1,10 @@
+import Footer from "./components/commen/Footer"
 import Navbar from "./components/commen/Navbar"
 const App = () => {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   )
 }
